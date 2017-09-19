8ab2cc09766aec78966ee80bb8ebb55d89296311
brian51665 <bdavis51892@yahoo.com>
2017-09-19 19:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // write your code below!
 console.log("Hello World!!!!")
+
+var dog = "Satisfied"
+
+if (dog === "Hungry") {
+  console.log("nomnomnomnomnom")
+} else {
+  console.log("zzzzzzzzzzzzzzz")
+}
